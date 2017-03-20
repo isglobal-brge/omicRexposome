627077809c299f8223185f11a7dbaeb507fb44b5
Carles Hernandez-Ferrer <kuragari.ch@gmail.com>
2017-03-20 18:22:16 +0100

    ..

--- R/omicRexposome.R
+++ R/omicRexposome.R
@@ -9,12 +9,11 @@
 #'
 #' @importClassesFrom MultiDataSet MultiDataSet
 #'
-#' @importFrom MultiDataSet commonSamples
+#' @importFrom MultiDataSet commonSamples betas getMs
 #' @importFrom Biobase sampleNames phenotypeNames pData fData
 #' @importFrom rexposome exposureNames
 #' @importFrom sva num.sv sva
 #' @importFrom limma lmFit eBayes
-#' @importFrom MEAL betas getMs DARegion DAProbe
 #' @importFrom ggplot2 theme theme_bw
 #' @importFrom ggplot2 aes geom_polygon geom_point geom_hline geom_vline
 #' @importFrom ggplot2 scale_colour_manual
